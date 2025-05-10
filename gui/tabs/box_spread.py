a65af1d16b9c94daa7c4f4627a7313222bc893af
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-05-10 03:06:05 +0200

    widgets added

--- gui/tabs/box_spread.py
+++ gui/tabs/box_spread.py
@@ -6,7 +6,7 @@ from time import sleep
 
 from PySide6.QtCore import Qt
 from PySide6.QtGui import QFont
-from PySide6.QtWidgets import QLabel, QTableWidgetItem
+from PySide6.QtWidgets import QLabel, QTableWidgetItem, QGraphicsOpacityEffect
 
 from core import Core, CoreDistributor
 
@@ -19,21 +19,42 @@ class BoxSpread:
 
         self.core: Core = CoreDistributor.get_core()
 
-        self.tab_registry = self.core.widget_registry['beta_weighted_deltas']
+        self.tab_registry = self.core.widget_registry['box_spread']
 
-        self.register_button_clicks()
+        self.register_events()
 
-        self.selection_options: dict[str, dict[str, str]] = self.load_selection_options()
+        self.selection_options: dict[str, dict[str, dict[str, str]]] = self.load_selection_options()
         self.box_spread_type = BoxSpreadType.LEND
         self.box_spread_type_btn(change=False)
 
         self.handle_widgets()
 
+    def register_events(self):
+        self.tab_registry['btn_type'].clicked.connect(lambda: self.box_spread_type_btn(change=True))
+        self.tab_registry['comboBox_currency'].currentIndexChanged.connect(lambda: self.currency_selected())
+
+        self.tab_registry['comboBox_index'].currentIndexChanged.connect(lambda: self.index_selected())
+
+        self.tab_registry['slider_rate'].sliderMoved.connect(lambda x: print(123, x))
+        self.tab_registry['slider_rate'].setEnabled(False)
+
+        self.opacity_effect = QGraphicsOpacityEffect(self.tab_registry['slider_rate']).setOpacity(0.25)
+        self.tab_registry['slider_rate'].setGraphicsEffect(self.opacity_effect)
+
+        
+    def currency_selected(self):
+        selected_currency = self.tab_registry['comboBox_currency'].currentText()
+
+        self.tab_registry['comboBox_index'].clear()
+        self.tab_registry['comboBox_index'].addItems(self.selection_options['currencies'][selected_currency].keys())
+        self.tab_registry['comboBox_expiry'].clear()
+
+    def index_selected(self):
+        selected_index = self.tab_registry['comboBox_index'].currentText()
+        print(f'{selected_index} selected')
+
     def handle_widgets(self):
         self.set_currency_options()
-
-    def register_button_clicks(self):
-        self.core.widget_registry['box_spread']['btn_type'].clicked.connect(lambda: self.box_spread_type_btn(change=True))
 
     def box_spread_type_btn(self, change: bool = False):
         if change:
@@ -42,13 +63,13 @@ class BoxSpread:
             else:
                 self.box_spread_type = BoxSpreadType.LEND
 
-        self.core.widget_registry['box_spread']['btn_type'].setText(self.box_spread_type.value)
+        self.tab_registry['btn_type'].setText(self.box_spread_type.value)
         btn_color = 'green' if self.box_spread_type == BoxSpreadType.LEND else 'red'
-        self.core.widget_registry['box_spread']['btn_type'].setStyleSheet(f'background-color: {btn_color};')
+        self.tab_registry['btn_type'].setStyleSheet(f'background-color: {btn_color};')
 
     def set_currency_options(self):
-        self.core.widget_registry['box_spread']['comboBox_currency'].clear()
-        self.core.widget_registry['box_spread']['comboBox_currency'].addItems(self.selection_options['currencies'].keys())
+        self.tab_registry['comboBox_currency'].clear()
+        self.tab_registry['comboBox_currency'].addItems(self.selection_options['currencies'].keys())
 
     @staticmethod
     def load_selection_options():
