0e3e7790d6239e788421dddac3d96faecc1fdf74
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-06-02 00:32:28 +0200

    streamlined var god-class

--- core.py
+++ core.py
@@ -51,51 +51,35 @@ class Core:
                 except ValueError:
                     print('Invalid input. Please enter a number.')
 
-            config = config['profiles'][list(config['profiles'].keys())[selection]]
+            profile = config['profiles'][list(config['profiles'].keys())[selection]]
         else:
-            config = config['profiles'][list(config['profiles'].keys())[0]]
-        try:
-            cls.HOST_IP = config['api_host_ip']
-            cls.API_PORT = config['api_port']
-            cls.CLIENT_ID = config['api_client_id']
-            cls.ACCOUNT_ID = config['ibkr_account_id']
-            cls.BENCHMARK = config['beta_benchmark']
-            cls.BETA_PERIOD = config['beta_period']
+            profile = config['profiles'][list(config['profiles'].keys())[0]]
 
+        try:
+            cls.HOST_IP = profile['api_host_ip']
+            cls.API_PORT = profile['api_port']
+            cls.CLIENT_ID = profile['api_client_id']
+            cls.ACCOUNT_ID = profile['ibkr_account_id']
+            cls.BENCHMARK = profile['beta_benchmark']
+            cls.BETA_PERIOD = profile['beta_period']
         except KeyError as e:
             raise KeyError(f'Missing key in config file: {e}')
 
+        cls.settings: dict[str, int] = config['settings']['beta_weighted_deltas']
+
     @classmethod
     def create_var_space(cls):
-        #TODO: Move as much as possible away from globalish space
 
         cls.threading_events: defaultdict[str, threading.Event] = defaultdict()
 
-        cls.update_intervals: dict[str, int] = {'beta_weighted_deltas': 60}  # in secs
-
         cls.account_list: list[str] = []
-        cls.active_tab: Tabs = Tabs.HOME
-
-        cls.raw_positions: dict[str, dict[str, any]] = {}
-
-        cls.positions: list[ContractInstance] = []
-        cls.positions_str_sorted: list[str] = []
-
-        cls.pos_betas: dict[str, float] = {}
 
         cls.table_contents: dict[str, list[str | float]] = {}
-
-        cls.item_register: dict[str, QtObj] = {}
-        cls.underlying_prices: dict[str, float] = {}
-
-        cls.bwd_update_refresh: QTFunc = None
-
-        cls.bxs_index_contract: dict[str, ibContract] = {}
 
         cls.widget_registry: dict[str, dict[str, QtObj]] = {}
 
         cls.tab_instances: dict[str, 'SubClassInstance'] = {}
-        #TODO: Global event trigger class to handle services <-> gui interactions
+
         cls.project_font = None
 
 
