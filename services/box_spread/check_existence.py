8f23f59efefd7a7aaa592b5d03b0ed0af61a911d
rudizabudi <40042979+rudizabudi@users.noreply.github.com>
2025-07-03 23:18:21 +0200

    bxs strike selectors added

--- services/box_spread/check_existence.py
+++ services/box_spread/check_existence.py
@@ -8,36 +8,37 @@ from threading import Event
 
 from core import ReqId, CoreDistributor
 from services.tws_api import TWSConDistributor
-from services.box_spread.request_expiries import BXSOptionChainData
-from services.contracts import create_expiry_dummy_contract
+from services.box_spread.requests_misc import BXSOptionChainData
+from services.contracts import build_expiry_dummy_contract
 
 
 class UpdateGuiStrikes(QObject):
-    trigger_strike_update = Signal(list)
+    trigger_strike_update = Signal(ibContract)
 
     def __init__(self):
         super().__init__()
 
-    def start(self, current_contract, expiry_date):
-        data = ContractExistence.get_valid_strikes(current_contract, expiry_date)
-        self.trigger_strike_update.emit(data)
+    def fire(self, current_contract: ibContract):
+        self.trigger_strike_update.emit(current_contract)
 
 
-class ContractExistence():
-    existing_contracts = defaultdict(bool)
+class ContractExistence:
+    existing_contracts = defaultdict()
     core = None
 
     @classmethod
-    def set_existence(cls, contract: ibContract, error: bool = False, *args, **kwargs):
+    def register_contract(cls, contract: ibContract):
+        cls.existing_contracts[contract] = None
+
+    @classmethod
+    def set_existence(cls, contract: ibContract, error: bool = False, register: bool = False, *args, **kwargs):
         if cls.core is None:
             cls.core = CoreDistributor.get_core()
 
-        if error:
-            cls.existing_contracts[contract] = False
-        else:
-            cls.existing_contracts[contract] = True
+        cls.existing_contracts[contract] = not error
 
-        if cls.core.threading_events['check_contract_existence']:
+        if None not in cls.existing_contracts.values() and cls.core.threading_events['check_contract_existence']:
+            print('Event set')
             cls.core.threading_events['check_contract_existence'].set()
 
     @classmethod
@@ -57,35 +58,37 @@ class ContractExistence():
         return valid_strikes
 
     @classmethod
-    def get_validity_status(cls, opt_contract: ibContract, strike: float) -> bool:
-        if cls.existing_contracts[opt_contract]:
+    def contract_checked(cls, opt_contract: ibContract) -> bool:
+        #if cls.existing_contracts.get(opt_contract, None) is not None:
+        if opt_contract in cls.existing_contracts.keys():
             return True
         return False
 
 
 def check_strikes(current_contract: ibContract, expiry_date: datetime, bxs_instance: "BoxSpread"):
+    print('Checking for strike existence started', datetime.now(), len(ContractExistence.existing_contracts.keys()))
+
     tws_con = TWSConDistributor.get_con()
     core = CoreDistributor.get_core()
-    tab_trigger = bxs_instance.tab_trigger['strikes']
 
-    for strike in BXSOptionChainData.strikes[current_contract.symbol]:
-        opt_contract_dummy = create_expiry_dummy_contract(index_contract=current_contract,
-                                                             expiry_date=expiry_date,
-                                                             strike=strike)
-
-        if ContractExistence.get_validity_status(opt_contract=opt_contract_dummy, strike=strike):
+    core.threading_events['check_contract_existence'] = Event()
+    existence_requested = False
+    print(f'Len old strikes: {len(BXSOptionChainData.strikes[current_contract.selected_type])}')
+    for strike in BXSOptionChainData.strikes[current_contract.selected_type]:
+        opt_contract_dummy = build_expiry_dummy_contract(index_contract=current_contract,
+                                                         expiry_date=expiry_date,
+                                                         strike=strike)
+        if ContractExistence.contract_checked(opt_contract=opt_contract_dummy):
             continue
 
-        price_callback = partial(ContractExistence.set_existence, contract=opt_contract_dummy)
+        existence_requested = True
 
-        core.threading_events['check_contract_existence'] = Event()
+        price_callback = partial(ContractExistence.set_existence, contract=opt_contract_dummy)
+        ContractExistence.register_contract(opt_contract_dummy)
         tws_con.reqContractDetails(ReqId.register_reqId(price_callback), opt_contract_dummy)
+
+    if existence_requested:
         core.threading_events['check_contract_existence'].wait()
 
-    tab_trigger.start(current_contract, expiry_date)
-    #ContractExistence.gui_update_strikes(current_contract, expiry_date)
+    bxs_instance.tab_trigger['strikes'].fire(current_contract)
 
-    # strikes = ContractExistence.get_valid_strikes(current_contract, expiry_date)
-    # print('Existence checked')
-    # bxs_instance.set_strike_comboBoxes(current_contract, strikes)
-    # print('Strikes updated')
